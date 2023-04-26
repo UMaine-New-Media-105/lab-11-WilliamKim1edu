1b354acff14c8986174e9206541897fc6da5f1c8
WilliamKim1edu <124187413+WilliamKim1edu@users.noreply.github.com>
2023-04-26 12:41:56 -0400

    https://editor.p5js.org/william.kim1/sketches/aaKdpmwGe

---
+++ asteroidsLab.js
@@ -0,0 +1,45 @@
+/*
+create array denoting number of asteroids
+asteroidCount = [];
+create array denoting asteroid sizes
+asteroidSizes = [small, medium, large];
+
+function setup() {
+  rectMode(CENTER);
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(0);
+}
+
+class spaceship
+constructor {
+give variable for , y, and speed
+}
+movement {
+make ship move with arrow keys
+ship movement should be slippery to simulate being in space
+}
+hit {
+shit explodes when hit by an asteroid
+}
+show {
+ship is a triangle
+}
+
+class asteroid
+constructor {
+give variable for x, y, size, and speed
+}
+movement {
+make asteroids randomly fly around
+have asteroids loop around the screen
+}
+hit {
+make asteroids split into two asteroids that are one size smaller when hit
+}
+show {
+use randomized strokes to give each asteroid a unique appearance
+}
+*/
